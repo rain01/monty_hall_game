c74272e09063d9dc535d86afd0d4ea7a3f016325
Rain <rain@fundcogito.com>
2020-05-31 16:44:45 -0700

    update for python version 3.8, fix a result bug, smaller changes

--- game.py
+++ game.py
@@ -1,87 +1,97 @@
 from random import shuffle
 import sys
 
-# created some time in 2015
-class door_game:
 
-	games_played, games_won, choice, doors = 0, 0, '1', {}
-
-
-	def __init__(self):
-		self.start_game()
+def main():
+    door_game = DoorGame()
+    door_game.start_game()
 
 
-	def start_game(self):
-		random_door = [1, 0, 0]
-		shuffle(random_door)
-		self.doors = {'1': random_door[0], '2': random_door[1], '3': random_door[2]}
-		
-		self.give_door_choice()
-		
-		self.remove_losing_door(1)
+class DoorGame:
+    def __init__(self):
+        self.games_played = 0
+        self.games_won = 0
+        self.choice = '1'
+        self.doors = {}
 
-		self.give_change_choice()
+    def start_game(self):
+        random_door = [1, 0, 0]
+        shuffle(random_door)
+        self.doors = {
+            '1': random_door[0],
+            '2': random_door[1],
+            '3': random_door[2]
+        }
+        
+        self.give_door_choice()
+        
+        self.remove_losing_door()
 
-		self.show_results()
+        self.give_change_choice()
 
-		self.play_again()
+        self.show_results()
+
+        self.play_again()
+
+    def give_door_choice(self):
+        self.choice = input('Please choose a door[1, 2, 3]: ')
+
+        if self.choice not in self.doors:
+            print(f' --- Door number {self.choice} does not exist.')
+            sys.exit()
+
+        print(' --- You chose door number', self.choice)
+
+    def remove_losing_door(self, remove='1'):
+        # make sure not to remove selected door and not the price door
+        if self.choice != remove and self.doors[remove] == 0:
+            print(' --- Removing a losing door.')
+            del self.doors[remove]
+        else:
+            # otherwise try the next door
+            remove = int(remove)
+            remove += 1
+            self.remove_losing_door(str(remove))
+            return
+
+        print(' --- Removed door number', remove)
+
+    def give_change_choice(self):
+        change = input('Would you like to choose a different door? [Y/n]: ').strip() or 'y'
+
+        while change.lower() not in ['y', 'n']:
+            change = input(' --- Incorrect input. Try again...[Y/n]: ').strip() or 'y'
+
+        if change.lower() == 'y':
+            print(' --- You chose a different door.')
+            del self.doors[self.choice]
+            self.choice = None
+
+    def show_results(self):
+        # if selection wasn't changed
+        if self.choice:
+            value = self.doors[self.choice]
+        else:
+            # if selection was changed, select the last leftover selection
+            key, value = self.doors.popitem()
+
+        if value == 1:
+            print(' --- You won! :)')
+            self.games_won += 1
+        else:
+            print(' --- You lost! :(')
+
+        self.games_played += 1
+        print(f' --- Games played: {self.games_played}. Games won: {self.games_won}')
+
+    def play_again(self):
+        try_again = input('Play again? [Y/n]: ').strip() or 'y'
+
+        if try_again.lower() == 'y':
+            self.start_game()
+        else:
+            print(' --- Bye!')
 
 
-	def give_door_choice(self):
-		self.choice = raw_input("Please choose a door[1, 2, 3]: ")
-		#choice = int(choice)
-		if self.choice not in self.doors:
-			print ' --- Door number ' + self.choice + ' does not exist.'
-			sys.exit()
-
-		print ' --- You chose door number', self.choice
-
-
-	def remove_losing_door(self, remove):
-		remove = str(remove)
-		if self.choice != remove and self.doors[remove] == 0:
-			print ' --- Removing a losing door.'
-			del self.doors[remove]
-		else:
-			remove = int(remove)
-			remove += 1
-			self.remove_losing_door(remove)
-			return
-
-		print ' --- Removed door number', remove
-
-
-	def give_change_choice(self):
-		change = raw_input("Would you like to choose a different door? [y, n]: ")
-
-		if change not in ['y', 'n']:
-			print ' --- Try again.'
-			sys.exit()
-
-		if change == 'y':
-			print ' --- You chose a different door.'
-			del self.doors[self.choice]
-
-
-	def show_results(self):
-		key, value = self.doors.popitem()
-		if value == 1:
-			print ' --- You won! :)'
-			self.games_won += 1
-		else:
-			print ' --- You lost! :('
-
-		self.games_played += 1
-		print ' --- Games played:' + str(self.games_played) + '. Games won: ' + str(self.games_won)
-
-
-	def play_again(self):
-		try_again = raw_input("Play again? [y, n]: ")
-
-		if try_again == 'y':
-			self.start_game()
-		else:
-			print ' --- Bye!'
-
-
-door_game()
+if __name__ == '__main__':
+    main()
